a4323052f10f9abd2d98be95d3bd5b5713adf867
Kasliwal <SK050608@cerner.net>
2016-12-25 10:28:45 -0500

    make it better

---
+++ src/main/java/LoanCalculatorApp.java
@@ -0,0 +1,25 @@
+class LoanCalculatorApp {
+	private String inputLeft, inputRight, calcEquals, inputOperator;
+	
+	public LoanCalculatorApp(String inpLft, String inpRgt, String calcEq, String inpOp){
+		inputLeft = inpLft;
+		inputRight = inpRgt;
+		calcEquals = calcEq;
+		inputOperator = inpOp;
+	}
+
+	public String calculate(){
+		if(calcEquals != null && calcEquals.equals("+")){
+			Float inpLeft = Float.valueOf(inputLeft);
+			Float inpRight = Float.valueOf(inputRight);
+
+			return new String(inpLeft + inpRight);
+		}
+	}
+
+	public static void main(String[] args){
+		LoanCalculatorApp lca = new LoanCalculatorApp(args[0], args[1], args[2], args[3]);
+		System.out.println(lca.calculate());
+
+	}
+}
