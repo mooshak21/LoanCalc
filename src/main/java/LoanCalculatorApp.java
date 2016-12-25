bb7735edd19e8693d3a2fdc779a862c2349b9871
Kasliwal <SK050608@cerner.net>
2016-12-25 10:56:52 -0500

    make it better

--- src/main/java/LoanCalculatorApp.java
+++ src/main/java/LoanCalculatorApp.java
@@ -9,19 +9,37 @@ class LoanCalculatorApp {
 	}
 
 	public String calculate(){
+		Float inpLeft = Float.valueOf(inputLeft);
+		Float inpRight = Float.valueOf(inputRight);
 		if(calcEquals != null && calcEquals.equals("+")){
-			Float inpLeft = Float.valueOf(inputLeft);
-			Float inpRight = Float.valueOf(inputRight);
-
 			return String.valueOf(new Float(inpLeft + inpRight));
+		}else if(calcEquals != null && calcEquals.equals("-")){
+			return String.valueOf(new Float(inpLeft - inpRight));
+		}else if(calcEquals != null && calcEquals.equals("*")){
+			return String.valueOf(new Float(inpLeft * inpRight));
+		}else if(calcEquals != null && calcEquals.equals("/")){
+			return String.valueOf(new Float(inpLeft / inpRight));
+		}else if(calcEquals != null && calcEquals.equals("%")){
+			return String.valueOf(new Float(inpLeft * inpRight / 100));
 		}
 
 		return "No Value";
 	}
 
 	public static void main(String[] args){
-		LoanCalculatorApp lca = new LoanCalculatorApp("169125", "12467","+","=");
-		System.out.println(lca.calculate());
+		LoanCalculatorApp lcaSalaryBonus = new LoanCalculatorApp("169125", "12467","+","=");
+		System.out.println(lcaSalaryBonus.calculate());
 
+		LoanCalculatorApp lcaSalaryIncrement = new LoanCalculatorApp("169125", "2.5", "%", "=");
+		System.out.println(lcaSalaryIncrement.calculate());
+
+		LoanCalculatorApp lcaSalaryLoss = new LoanCalculatorApp("169125", "19125", "-", "=");
+		System.out.println(lcaSalaryLoss.calculate());
+
+		LoanCalculatorApp lcaSalaryPayCheck = new LoanCalculatorApp("169125", "26", "/", "=");
+		System.out.println(lcaSalaryPayCheck.calculate());
+
+		LoanCalculatorApp lcaSalaryPayIncrease = new LoanCalculatorApp("6667", "26", "*", "=");
+		System.out.println(lcaSalaryPayIncrease.calculate());
 	}
 }
