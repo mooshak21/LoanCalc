bb4b87e864b6f8370bf8f12800a13988dceeb1a0
Gagan Jain <Gagan.Jain@us.qbe.com>
2016-12-27 13:48:59 -0500

    make it better

--- src/main/java/calculator/LoanController.java
+++ src/main/java/calculator/LoanController.java
@@ -1,3 +1,9 @@
+package calculator;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class LoanController {
 
