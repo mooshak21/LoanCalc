0870166244accf5c8102046d2bbae90916ccf083
jaingagan <jain_gagan@yahoo.com>
2016-12-29 10:28:51 -0500

    make it better

--- src/main/java/calculator/LoanCalculatorController.java
+++ src/main/java/calculator/LoanCalculatorController.java
@@ -10,13 +10,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class LoanCalculatorController {
 
-	    @RequestMapping(value="/loan", method=RequestMethod.GET),
+	    @RequestMapping(value="/loan", method=RequestMethod.GET)
+	        public String loan(
 		@RequestParam("airVal") String airVal,
 		@RequestParam("lender") String lender,
 		@RequestParam("loanAmt") String loanAmt,
 		@RequestParam("state") String state,
-		@RequestParam("numOfYears") String numOfYears
-	        public String loan(Model model) {
+		@RequestParam("numOfYears") String numOfYears, Model model) {
 			        model.addAttribute("message", new LoanCalculatorApp().loan(airVal, loanAmt, numOfYears, lender, state).toString());
 			        return "loan";
 		    }
