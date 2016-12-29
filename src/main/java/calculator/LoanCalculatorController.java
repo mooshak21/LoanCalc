d9ebf9ce7bb495843de87ecaa9042b524b8b4232
jaingagan <jain_gagan@yahoo.com>
2016-12-29 13:15:20 -0500

    make it better

--- src/main/java/calculator/LoanCalculatorController.java
+++ src/main/java/calculator/LoanCalculatorController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.client.RestTemplate;
-import javax.util.HashMap;
-import javax.util.Map;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class LoanCalculatorController {
