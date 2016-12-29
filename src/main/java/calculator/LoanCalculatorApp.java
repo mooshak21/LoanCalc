5f13664a8aefca31061bad91e73be3749da67f77
jaingagan <jain_gagan@yahoo.com>
2016-12-29 17:54:41 -0500

    make it better

--- src/main/java/calculator/LoanCalculatorApp.java
+++ src/main/java/calculator/LoanCalculatorApp.java
@@ -1,6 +1,6 @@
 package calculator;
 
-import org.springframework.stereotype.RestController;
+import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMethod;
