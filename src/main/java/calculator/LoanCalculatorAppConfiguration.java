774088e9da62b8d12353416d1004ab4fc1d4e02f
jaingagan <jain_gagan@yahoo.com>
2016-12-30 09:37:10 -0500

    make it better

--- src/main/java/calculator/LoanCalculatorAppConfiguration.java
+++ src/main/java/calculator/LoanCalculatorAppConfiguration.java
@@ -1,4 +1,4 @@
-package com.websystique.springmvc.configuration;
+package calculator;
  
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
