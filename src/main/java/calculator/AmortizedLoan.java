50b28665b320c6097bd4a127bd383c49fc813e24
jaingagan <jain_gagan@yahoo.com>
2017-01-09 10:38:18 -0500

    make it better

--- src/main/java/calculator/AmortizedLoan.java
+++ src/main/java/calculator/AmortizedLoan.java
@@ -14,9 +14,9 @@ public class AmortizedLoan extends Loan {
 	public AmortizedLoan(double mnthly, double amt, double tot, String lndr, String st, double intRate, double Apr, int numYears, double interestPayment){
 		super(mnthly, amt, tot, lndr, st, intRate, Apr, numYears, interestPayment);
 		int maxCmpPeriod = numYears*12;
-		Calendar dateEntry = Calendar.getInstance();
-		dateEntry.setTimeInMillis(System.currentTimeMillis());
 		for(int cmpPeriod = 1; cmpPeriod < maxCmpPeriod; cmpPeriod++){
+			Calendar dateEntry = Calendar.getInstance();
+			dateEntry.setTimeInMillis(System.currentTimeMillis());
 			dateEntry.set(dateEntry.get(Calendar.YEAR),dateEntry.get(Calendar.MONTH)+cmpPeriod, dateEntry.get(Calendar.DAY_OF_MONTH));
 			entries.put(new Integer(cmpPeriod), calculateLoanEntry(dateEntry, amt, numYears, cmpPeriod % maxCmpPeriod, intRate, Apr));
 		}
