ebf9c8090fbb9d5b1f28e71e894c968b8a283f17
jaingagan <jain_gagan@yahoo.com>
2017-01-09 13:22:07 -0500

    make it better

--- src/main/java/calculator/AmortizedLoan.java
+++ src/main/java/calculator/AmortizedLoan.java
@@ -22,7 +22,7 @@ public class AmortizedLoan extends Loan {
 			dateEntry.set(dateEntry.get(Calendar.YEAR),dateEntry.get(Calendar.MONTH)+cmpPeriod-1, dateEntry.get(Calendar.DAY_OF_MONTH));
 			LoanEntry loanEntry = calculateLoanEntry(dateEntry, amt, numYears, cmpPeriod % maxCmpPeriod, intRate, Apr);
 
-			if(monthly <= loanEntry.getPrincipal())
+			if(mnthly <= loanEntry.getPrincipal())
 				entries.put(new Integer(cmpPeriod), loanEntry);
 	
 		}
