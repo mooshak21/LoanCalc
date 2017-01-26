77e1ed77d9d883c289f5d682b2e9bb3fa0e48e99
jaingagan <jain_gagan@yahoo.com>
2017-01-26 11:34:31 -0500

    make it better

--- src/main/java/com/ayushi/loan/service/LendingService.java
+++ src/main/java/com/ayushi/loan/service/LendingService.java
@@ -2,11 +2,13 @@ package com.ayushi.loan.service;
 
 import com.ayushi.loan.exception.LoanAccessException;
 import com.ayushi.loan.Loan;
+import java.util.List;
+import java.io.Serializable;
 
 public interface LendingService {
 	public void createLoan(Loan loan) throws LoanAccessException;
 	public Loan retrieveLoan(Loan loan) throws LoanAccessException;
 	public void modifyLoan(Loan loan) throws LoanAccessException;
 	public void removeLoan(Loan loan) throws LoanAccessException;
-	public List<Loan> findLoan(String query, Object[] objVals) throws LoanAccessException;
+	public List<Serializable> findLoan(String query, Object[] objVals) throws LoanAccessException;
 }
