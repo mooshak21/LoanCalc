11e2581de95a25da7138e37c7d6a97ba63f18530
jaingagan <jain_gagan@yahoo.com>
2017-01-26 11:18:14 -0500

    make it better

--- src/main/java/com/ayushi/loan/exception/LoanAccessException.java
+++ src/main/java/com/ayushi/loan/exception/LoanAccessException.java
@@ -1,6 +1,6 @@
 package com.ayushi.loan.exception;
 
-import org.springframework.dao.DataAccessException
+import org.springframework.dao.DataAccessException;
 
 public class LoanAccessException extends Exception {
 	public LoanAccessException(DataAccessException dae){
