e7266fadcc992b7a9e203ab3535dc1e80d2a9d06
jaingagan <jain_gagan@yahoo.com>
2017-01-30 17:16:39 -0500

    make it better

---
+++ src/main/java/com/ayushi/loan/service/PreferenceAttributeService.java
@@ -0,0 +1,16 @@
+package com.ayushi.loan.service;
+
+import com.ayushi.loan.exception.PreferenceAccessException;
+import com.ayushi.loan.Loan;
+import java.util.List;
+import java.io.Serializable;
+
+public interface PreferenceAttributeService {
+	public void createPreference(Preference preference) throws PreferenceAccessException;
+	public Preference retrievePreference(Preference preference) throws PreferenceAccessException;
+	public void modifyPreference(Preference preference) throws PreferenceAccessException;
+	public void removePreference(Preference preference) throws PreferenceAccessException;
+	public List<Serializable> findPreference(String query, Object[] objVals) throws PreferenceAccessException;
+	public void addPreferences(Loan loan, List<Integer> prefIds) throws PreferenceAccessException;
+	public List<Integer> processPreferences(List<CheckPreference> prefs, Predicate<CheckPreference> tester);
+}
---
+++ src/main/java/com/ayushi/loan/service/PreferenceService.java
@@ -0,0 +1,42 @@
+package com.ayushi.loan.service;
+
+import com.ayushi.loan.dao.PreferenceDao;
+import com.ayushi.loan.exception.PreferenceAccessException;
+import com.ayushi.loan.Loan;
+import java.util.List;
+import java.io.Serializable;
+
+public class PreferenceService implements PreferenceAttributeService {
+	private PreferenceDao preferenceDao;
+
+	public PreferenceService(PreferenceDao preferenceDao){
+		this.preferenceDao = preferenceDao;
+	}
+	public void setPreferenceDao(PreferenceDao preferenceDao){
+		this.preferenceDao = preferenceDao;
+	}
+	public PreferenceDao getPreferenceDao(){
+		return preferenceDao;
+	}
+	public void createPreference(Preference preference) throws PreferenceAccessException {
+		preferenceDao.insert(preference);
+	}
+	public Preference retrievePreference(Preference preference) throws PreferenceAccessException {
+		return (Preference)preferenceDao.find(Preference.class, preference);
+	}
+	public void modifyPreference(Preference preference) throws PreferenceAccessException {
+		preferenceDao.update(preference);
+	}
+	public void removePreference(Preference preference) throws PreferenceAccessException {
+		preferenceDao.remove(preference);
+	}
+	public List<Serializable> findPreference(String query, Object[] objVals) throws PreferenceAccessException {
+		return (List<Serializable>) preferenceDao.find(query, objVals);
+	}
+	public void addPreferences(Loan loan, List<Integer> prefIds) throws PreferenceAccessException {
+		preferenceDao.insert(loan, prefIds);
+	}
+	public List<Integer> processPreferences(Preferences preferences, Predicate<CheckPreference> tester){
+		return Preferences.processPreferencesWithPredicate(preferences.getPreferences(), tester);
+	}
+}
