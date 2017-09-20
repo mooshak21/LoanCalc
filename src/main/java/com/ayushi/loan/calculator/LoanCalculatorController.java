38633d9437b66a0fcf7112360db74b2de916919c
jaingagan <jain_gagan@yahoo.com>
2017-09-20 16:00:53 -0400

    view loan and view loan entries corrected with not null

--- src/main/java/com/ayushi/loan/calculator/LoanCalculatorController.java
+++ src/main/java/com/ayushi/loan/calculator/LoanCalculatorController.java
@@ -437,12 +437,12 @@ public class LoanCalculatorController{
 			AmortizedLoan al = null;
                         String amortizeOn = (String) model.asMap().get("amortizeOn");
                         String payoffOn = (String) model.asMap().get("payoffOn");
-                        if(payoffOn.isEmpty()){
+                        if(payoffOn != null && payoffOn.isEmpty()){
                             payoffOn = calTodayStr;
                         }
 			if(loans != null){
 				Loan loan = (Loan)loans.get(pageid-1);
-				if(al != null){
+				if(al != null && amortizeOn != null){
 	                                al = new AmortizedLoan(amortizeOn, loan.getMonthly(), loan.getAmount(), loan.getTotal(), loan.getLender(), loan.getState(), 
                                         loan.getInterestRate(), loan.getAPR(), loan.getNumberOfYears(), 0);
 					model.addAttribute("payoffAmt", al.getPayoffAmount(loan.getAmount(), payoffOn));
@@ -451,9 +451,6 @@ public class LoanCalculatorController{
 				}
 			}
 			
-//			model.addAttribute("payoffOn", calTodayStr);		
-//			model.addAttribute("amortizeOn", calTodayStr);		
-
 			return "viewloan";
 		   }
 	  
