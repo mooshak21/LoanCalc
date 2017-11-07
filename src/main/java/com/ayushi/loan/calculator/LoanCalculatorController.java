378d1346fb7425a1328ff5e278b71a21852bf46e
jaingagan <jain_gagan@yahoo.com>
2017-11-06 21:18:31 -0500

    removed preference objects to preference iteration

--- src/main/java/com/ayushi/loan/calculator/LoanCalculatorController.java
+++ src/main/java/com/ayushi/loan/calculator/LoanCalculatorController.java
@@ -611,7 +611,7 @@ public class LoanCalculatorController implements ServletContextAware {
 
 	if(prefs != null){
 	    String[] prefAttrs = new String[] { "locationPreference", "webServicePreference", "riskTolerancePreference", "timeHorizonPreference",
-						  "loanAmt", "airVal", "lender", "state", "emailAddress", "loanId", "reminderFrequency" };	
+						  "loanAmt", "airVal", "lender", "state", "emailAddress", "loanId", "reminderFrequency", "test" };	
 	    prefVal = new ArrayList<String>(prefs.size());	
 	    prefAttr = new ArrayList<String>(prefs.size());
 	    int prefIdx = 0;
