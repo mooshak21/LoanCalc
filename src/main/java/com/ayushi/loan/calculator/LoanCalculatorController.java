38138a56e7de1cfd369dd43d24026f7f8dbbbc8b
jaingagan <jain_gagan@yahoo.com>
2017-12-09 17:20:41 -0500

    login crypt logic added

--- src/main/java/com/ayushi/loan/calculator/LoanCalculatorController.java
+++ src/main/java/com/ayushi/loan/calculator/LoanCalculatorController.java
@@ -1433,7 +1433,7 @@ private boolean updatePreferencePassword(String email, String newPassword) {
 													if(p instanceof PasswordPreference){
 														// gensalt's log_rounds parameter determines the complexity
 														// the work factor is 2**log_rounds, and the default is 10
-														String hashed = BCrypt.hashpw(password, BCrypt.gensalt(12));
+														String hashed = BCrypt.hashpw(password, BCrypt.gensalt());
 
 														// Check that an unencrypted password matches one that has
 														// previously been hashed
