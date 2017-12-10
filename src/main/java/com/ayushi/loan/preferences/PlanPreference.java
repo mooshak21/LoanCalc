c15fe9c45b880a2e6118c3ef34c31252cfbd4092
jaingagan <jain_gagan@yahoo.com>
2017-12-10 15:20:04 -0500

    Plan Preference added

---
+++ src/main/java/com/ayushi/loan/preferences/PlanPreference.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ayushi.loan.preferences;
+
+/**
+ *
+ * @author Daniel Gago
+ */
+public class PlanPreference extends Preference{
+
+    public PlanPreference() {
+        super();
+        this.type="Plan";
+        this.description="Plan";
+        this.name="PlanPreference";
+    }
+    
+    
+}
