94bc372ec6ad9192b199f31a95d620f9e1fee880
kkscwcd <kkscwcd@gmail.com>
2018-03-15 18:23:23 +0530

    Paypal integration

--- src/main/java/com/ayushi/loan/calculator/LoanCalculatorController.java
+++ src/main/java/com/ayushi/loan/calculator/LoanCalculatorController.java
@@ -2098,7 +2098,7 @@ public class LoanCalculatorController implements ServletContextAware {
                  * which specifies the total number of billing cycles in the regular payment period. If you either do not specify a value or specify the value 0,
                  * the payments continue until PayPal (or the buyer) cancels or suspends the profile
                  */
-                paymentDefinition.setCycles("12");
+                paymentDefinition.setCycles("0");
 
 // Currency
                 Currency currency = new Currency();
