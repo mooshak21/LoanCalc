3dea5d7572ed2fd9f7193369ad845d884aa92853
kkscwcd <kkscwcd@gmail.com>
2018-03-21 11:39:14 +0530

    Paypal issue

--- src/main/java/com/ayushi/loan/calculator/LoanCalculatorController.java
+++ src/main/java/com/ayushi/loan/calculator/LoanCalculatorController.java
@@ -2044,7 +2044,7 @@ public class LoanCalculatorController implements ServletContextAware {
         ApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/applicationContext.xml");
         PaymentService paymentService = (PaymentService) appCtx.getBean("paymentService");
 
-        SimpleDateFormat sdf=new SimpleDateFormat("dd-MM-yyyy");
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'");
 
         try {
             logger.info("Create Payment  "+paymentType);
@@ -2157,7 +2157,7 @@ public class LoanCalculatorController implements ServletContextAware {
                 agreement.setName("Base Agreement");
                 agreement.setDescription("Basic Agreement");
 
-                SimpleDateFormat payPalFormat=new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+                SimpleDateFormat payPalFormat=new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'");
 
                 agreement.setStartDate(payPalFormat.format(sdf.parse(paymentStartDate)));
 
