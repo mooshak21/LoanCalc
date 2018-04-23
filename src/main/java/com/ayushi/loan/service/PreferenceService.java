5834e38a9b0bb409167b3b7ca16bd2e0879406c4
Gagan Jain <Gagan.Jain@marsh.com>
2018-04-23 12:12:55 -0400

    transaction added

--- src/main/java/com/ayushi/loan/service/PreferenceService.java
+++ src/main/java/com/ayushi/loan/service/PreferenceService.java
@@ -10,6 +10,8 @@ import com.ayushi.loan.preferences.CheckPreference;
 import java.util.function.Predicate;
 import com.ayushi.loan.preferences.Preference;
 import com.ayushi.loan.preferences.Preferences;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.annotation.Propagation;
 
 public class PreferenceService implements PreferenceAttributeService {
 	private PreferenceDao preferenceDao;
@@ -23,24 +25,32 @@ public class PreferenceService implements PreferenceAttributeService {
 	public PreferenceDao getPreferenceDao(){
 		return preferenceDao;
 	}
+
+	@Transactional(value = "txManager", propagation = Propagation.REQUIRED, readOnly = false)
 	public void createPreference(Preference preference) throws PreferenceAccessException {
 		preferenceDao.insert(preference);
 	}
+	@Transactional(value = "txManager", propagation = Propagation.REQUIRED, readOnly = true)
        	public Preference retrievePreference(Preference preference) throws PreferenceAccessException {
 		return (Preference)preferenceDao.find(Preference.class, preference);
 	}
+	@Transactional(value = "txManager", propagation = Propagation.REQUIRED, readOnly = false)
 	public void modifyPreference(Preference preference) throws PreferenceAccessException {
 		preferenceDao.update(preference);
 	}
+	@Transactional(value = "txManager", propagation = Propagation.REQUIRED, readOnly = false)
 	public void removePreference(Preference preference) throws PreferenceAccessException {
 		preferenceDao.remove(preference);
 	}
+	@Transactional(value = "txManager", propagation = Propagation.REQUIRED, readOnly = true)
 	public List<Preference> findPreference(String query, Object[] objVals) throws PreferenceAccessException {
 		return (List<Preference>) preferenceDao.find(query, objVals);
 	}
+	@Transactional(value = "txManager", propagation = Propagation.REQUIRED, readOnly = true)
         public Preference findOnePreference(Integer id, String email) throws PreferenceAccessException {
             return (Preference) preferenceDao.find("select p from pref p where p.pref_emailAddress = ? and p.pref_id = ?", new Object[]{email,id}).get(0);
         }
+	@Transactional(value = "txManager", propagation = Propagation.REQUIRED, readOnly = false)
 	public void addPreferences(Loan loan, List<Integer> prefIds) throws PreferenceAccessException {
 		preferenceDao.insert(loan, prefIds);
 	}
