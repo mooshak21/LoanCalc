2737ea0606c1334d9869c0622aca0cad00884120
Gagan Jain <Gagan.Jain@marsh.com>
2018-04-23 13:00:42 -0400

    transaction removed

--- src/main/java/com/ayushi/loan/service/PreferenceService.java
+++ src/main/java/com/ayushi/loan/service/PreferenceService.java
@@ -15,7 +15,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 
 public class PreferenceService implements PreferenceAttributeService {
-	@Autowired
 	private PreferenceDao preferenceDao;
 
 	public PreferenceService(PreferenceDao preferenceDao){
@@ -30,35 +29,33 @@ public class PreferenceService implements PreferenceAttributeService {
 		return this.preferenceDao;
 	}
 	
-	@Transactional
 	public void createPreference(Preference preference) throws PreferenceAccessException {
 		preferenceDao.insert(preference);
 	}
-	@Transactional
        	public Preference retrievePreference(Preference preference) throws PreferenceAccessException {
 		return (Preference)preferenceDao.find(Preference.class, preference);
 	}
-	@Transactional
+
 	public void modifyPreference(Preference preference) throws PreferenceAccessException {
 		preferenceDao.update(preference);
 	}
-	@Transactional
+
 	public void removePreference(Preference preference) throws PreferenceAccessException {
 		preferenceDao.remove(preference);
 	}
-	@Transactional
+
 	public List<Preference> findPreference(String query, Object[] objVals) throws PreferenceAccessException {
 		return (List<Preference>) preferenceDao.find(query, objVals);
 	}
-	@Transactional
+
         public Preference findOnePreference(Integer id, String email) throws PreferenceAccessException {
             return (Preference) preferenceDao.find("select p from pref p where p.pref_emailAddress = ? and p.pref_id = ?", new Object[]{email,id}).get(0);
         }
-	@Transactional
+
 	public void addPreferences(Loan loan, List<Integer> prefIds) throws PreferenceAccessException {
 		preferenceDao.insert(loan, prefIds);
 	}
-	@Transactional
+
         public List<Preference> processPreferences(Preferences preferences, Predicate<Preference> tester) throws PreferenceProcessException{
 		return Preferences.processPreferencesWithPredicate(preferences.getPreferences(), tester);
 	}
