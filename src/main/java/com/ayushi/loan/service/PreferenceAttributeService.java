9ac9b607ade582daecfdd7eee4b203b92fcaebd3
jaingagan <jain_gagan@yahoo.com>
2018-05-06 14:47:23 -0400

    preferencedao updated with transactions

--- src/main/java/com/ayushi/loan/service/PreferenceAttributeService.java
+++ src/main/java/com/ayushi/loan/service/PreferenceAttributeService.java
@@ -2,7 +2,7 @@ package com.ayushi.loan.service;
 
 import com.ayushi.loan.exception.PreferenceAccessException;
 import com.ayushi.loan.exception.PreferenceProcessException;
-import com.ayushi.loan.Loan;
+//import com.ayushi.loan.Loan;
 import java.util.List;
 import java.io.Serializable;
 import com.ayushi.loan.preferences.CheckPreference;
@@ -17,7 +17,7 @@ public interface PreferenceAttributeService {
 	public void removePreference(Preference preference) throws PreferenceAccessException;
 	public List<Preference> findPreference(String query, Object[] objVals) throws PreferenceAccessException;
         public Preference findOnePreference(Integer id, String email) throws PreferenceAccessException;
-	public void addPreferences(Loan loan, List<Integer> prefIds) throws PreferenceAccessException;
+//	public void addPreferences(Loan loan, List<Integer> prefIds) throws PreferenceAccessException;
         public List<Preference> processPreferences(Preferences prefs, Predicate<Preference> tester) throws PreferenceProcessException;
 //	public List<Integer> processPreferences(Preferences prefs, Predicate<Preference> tester) throws PreferenceProcessException;
 }
