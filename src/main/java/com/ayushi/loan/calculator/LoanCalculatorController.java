c9e334f4194b232c0c482dc318b81822047ba7a8
jaingagan <jain_gagan@yahoo.com>
2020-06-16 11:23:42 -0400

    LoanCalculatorController.java

--- src/main/java/com/ayushi/loan/calculator/LoanCalculatorController.java
+++ src/main/java/com/ayushi/loan/calculator/LoanCalculatorController.java
@@ -1175,7 +1175,7 @@ public class LoanCalculatorController implements ServletContextAware {
 			lpwdPref.setName("Password");
 			lpwdPref.setEmailAddress(email);
 			// Hash a password for the first time
-			Byte bpasswordPreference = Byte.valueOF(passwordPreference);
+			Byte bpasswordPreference = Byte.valueOf(passwordPreference);
 			String hashed = BCrypt.hashpw(bpasswordPreference, BCrypt.gensalt(15));
 
 			lpwdPref.setValue(hashed);
