9ea8f3089bfa64edb2c6d7455390d0cd3750ea8b
jaingagan <jain_gagan@yahoo.com>
2020-07-20 15:26:14 -0400

    JWT removed

--- src/main/java/com/ayushi/loan/config/ConstantStrings.java
+++
@@ -1,8 +0,0 @@
-package com.ayushi.loan.config;
-
-public abstract class ConstantStrings {
-    public static final long ACCESS_TOKEN_VALIDITY_SECONDS = 1000 * 60 * 60  *10;//10 hours
-    public static final String SIGNING_KEY = "secretkey";
-    public static final String TOKEN_PREFIX = "Bearer ";
-    public static final String HEADER_STRING = "Authorization";
-}
