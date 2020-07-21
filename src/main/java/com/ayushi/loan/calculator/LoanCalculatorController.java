eb1791e70d5b0bd91ee28b3aef0c1909c72a968c
jaingagan <jain_gagan@yahoo.com>
2020-07-21 18:38:33 -0400

    Check Email and User Preferences removed

--- src/main/java/com/ayushi/loan/calculator/LoanCalculatorController.java
+++ src/main/java/com/ayushi/loan/calculator/LoanCalculatorController.java
@@ -1919,7 +1919,7 @@ public class LoanCalculatorController implements ServletContextAware {
 			model.addAttribute("userEmail", email);
 			boolean emailPasswordIgnoreFlag = false, emailPasswordFlag = false;
 			List<Preference> preferences = null;		
-			if(!password.equals("Ignore"))
+			if(!password.equalsIgnoreCase("Ignore"))
 				emailPasswordFlag = checkPreferenceEmailAddress(email, password);
 			else
 				emailPasswordIgnoreFlag = true;
